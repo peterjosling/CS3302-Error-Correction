43877adecf9ca3f1f160c0fd8ea247b72497ed77
Peter Josling <peterjosling@gmail.com>
2014-11-01 19:51:38 +0000

    Add initial HammingCode implementation and test

---
+++ src/uk/ac/standrews/s120001757/errorcorrection/HammingCode.java
@@ -0,0 +1,80 @@
+package uk.ac.standrews.s120001757.errorcorrection;
+
+public class HammingCode {
+	private static final int R_BOUND = 100;
+
+	private int r;
+	private boolean[][] parityCheck;
+	private boolean[][] generator;
+
+	public HammingCode(int r) {
+		this.r = r;
+
+		int length = (int)Math.pow(2, r) - 1;
+
+		parityCheck = new boolean[length][r];
+		generator = new boolean[length - r][length];
+
+		// Populate parity check matrix.
+		int currentRow = 0;
+		int currentIdentityRow = length - r;
+
+		for (int i = length - 1; i >= 0; i--) {
+			int row;
+
+			// If i is a power of two, it's part of the identity matrix and goes at the bottom.
+			if ((i & (i - 1)) == 0) {
+				row = currentIdentityRow++;
+			} else {
+				row = currentRow++;
+			}
+
+			for (int j = 0; j < r; j++) {
+				parityCheck[row][j] = (i & (1 << (r - 1 - j))) != 0;
+			}
+		}
+
+
+	}
+
+	public static int getOptimumR(double channelNoise, double maxCorruption) {
+		int r = 1;
+		double totalCorruption = 0;
+
+		while (totalCorruption < maxCorruption && r <= R_BOUND) {
+			r++;
+			totalCorruption = getCorruptionRate(r, channelNoise);
+		}
+
+		return r - 1;
+	}
+
+	public static double getInformationRate(int r) {
+		int length = (int)Math.pow(2, r) - 1;
+
+		return (double)(length - r) / length;
+	}
+
+	public static double getCorruptionRate(int r, double channelNoise) {
+		int length = (int)Math.pow(2, r) - 1;
+		double iChannelNoise = 1 - channelNoise;
+
+		// Proportion of corrupted bits is 1 - (probability of 0 corrupted + probability of 1 corrupted).
+		double probZeroErrors = Math.pow(iChannelNoise, length);
+		double probOneError = length * Math.pow(iChannelNoise, length - 1) * channelNoise;
+
+		return 1 - (probZeroErrors + probOneError);
+	}
+
+	public static int nCr(int n, int r) {
+		return factorial(n) / (factorial(n - r) * factorial(r));
+	}
+
+	public static int factorial(int n) {
+		if (n <= 1) {
+			return 1;
+		}
+
+		return n * factorial(n - 1);
+	}
+}
--- src/uk/ac/standrews/s120001757/errorcorrection/Main.java
+++ src/uk/ac/standrews/s120001757/errorcorrection/Main.java
@@ -4,4 +4,41 @@ public class Main {
 	public static void main(String[] args) {
 
 	}
+
+	public static void getOptimumR() {
+		double errorProb = 0.001;
+		double maxCorruption = 0.01;
+
+		System.out.println("Error probability: " + errorProb);
+		System.out.println("Max corruption: " + maxCorruption);
+
+		int r = HammingCode.getOptimumR(errorProb, maxCorruption);
+
+		if (r == 1) {
+			System.out.println("No hamming code meets these requirements.");
+			System.exit(0);
+		}
+
+
+
+
+
+		// Is this correct? Spec says proportion of DATA bits. Am I doing this, or looking at all bits?
+
+
+
+
+
+		System.out.println("Optimum r: " + r);
+		System.out.println("Information rate: " + HammingCode.getInformationRate(r));
+		System.out.println("Predicted corruption: " + HammingCode.getCorruptionRate(r, errorProb));
+	}
+
+	public static void encodeAndTransmit() {
+		double errorProb = 0.001;
+		int r = 3;
+		int length = 100;
+
+
+	}
 }
